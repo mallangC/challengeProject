7d9a968558e426dd06b6150aaf786fe5d755ad88
Tod <ckj9001@gmail.com>
2025-03-26 16:09:39 +0900

    init

---
+++ src/main/java/com/zerobase/challengeproject/ChallengeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.challengeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChallengeProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zerobase/challengeproject/ChallengeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.challengeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
