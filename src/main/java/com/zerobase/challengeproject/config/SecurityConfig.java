2e9475c345eadb0ba41972b432459b03bacd9b4b
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-04-01 11:50:32 +0900

    #3 #6 #7 #9 refactor:feature_challenge -> 예최처리 추가에 따라 테스트코드 변경

--- src/main/java/com/zerobase/challengeproject/config/SecurityConfig.java
+++ src/main/java/com/zerobase/challengeproject/config/SecurityConfig.java
@@ -26,10 +26,7 @@ public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
-    private final AuthenticationConfiguration authenticationConfiguration;
 
-    @Value("${spring.security.enabled:true}")  // 기본값 true
-    private boolean securityEnabled;
 
 
     @Bean
@@ -84,27 +81,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable);
-        /*
+                .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/member/sign-up", "/api/member/email-auth", "/api/member/login").permitAll()
                         .anyRequest().authenticated()
                 );
-*/
+
         http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.addFilterAt(jwtAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
-        if (!securityEnabled) {
-            // 🔥 Security 비활성화 (로컬 테스트용)
-            http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        } else {
-            // ✅ Security 활성화 (운영 환경)
-            http.authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/member/sign-up", "/api/member/email-auth", "/api/member/login").permitAll()
-                    .anyRequest().authenticated()
-            );
-            http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-        }
+
 
         return http.build();
     }
